7cc112e6f05a82eb81cb61450ec2c3cd4c65ce2f
Jonny McFadden <jmcfadden1528@gmail.com>
2019-03-04 13:31:01 -0600

    uncompleted

--- LoopMultiply/Program.cs
+++ LoopMultiply/Program.cs
@@ -6,13 +6,24 @@ namespace LoopMultiply
     {
         public static void Main()
         {
+            
             Console.Write("Enter a number: ");
+            string userNum = Console.ReadLine();
+            int number = Convert.ToInt32(userNum);
 
             // TODO: Loop from 2 to 20 and multiply the loop value by the number the user entered and output the value each time. 
+            //Convert.ToInt32
+            for (int counter = 2; counter < 21; counter++)
+            {
+                
+                Console.WriteLine(counter * number);
+            }
 
-            // TODO: When the loop is finished, output the sum of all previous values.
+
 
             Console.ReadLine();
         }
+
+
     }
 }
